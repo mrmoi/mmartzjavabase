6431cf4830e855ec89311b2304fc9721ac41e591
mrmoi <mlmart16@asu.edu>
2018-09-16 23:28:11 -0700

    latest updates

---
+++ src/main/java/com/mmartzbaseapp/MmartzbaseappApplication.java
@@ -0,0 +1,13 @@
+package com.mmartzbaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmartzbaseappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MmartzbaseappApplication.class, args);
+	}
+}
